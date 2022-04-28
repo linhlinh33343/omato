d36b8a10b8122dec742ea96428cf22bf30cc4cee
unknown <hathi5798@gmail.com>
2022-04-28 22:31:21 +0700

    upload source code

---
+++ asset/app/home.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+     
+    $(window).scroll(function() {
+        if($(this).scrollTop()){
+            $('header').addClass('sticky')
+        }
+        else{
+            $('header').removeClass('sticky')
+
+        }
+    })
+})
+
+new WOW().init();    
